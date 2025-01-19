9dfdf5235183205451bb5f9352b631b30fc54b48
chiphan1110 <chiphanphan03@gmail.com>
2025-01-19 23:31:48 +0700

    Add everything

--- scripts/story_adj_analysis.sh
+++ scripts/story_adj_analysis.sh
@@ -3,6 +3,6 @@ BASE_DIR="$(dirname "$PWD")"  # Cultural-Fairness-LLM
 
 python $BASE_DIR/generative/data-processing/calculate_odds_ratio.py \
      --input_folder $BASE_DIR/generative/extracted-adj \
-     --threshold 5 \
+     --threshold 10 \
      --output_folder $BASE_DIR/generative/extracted-adj/output_odds_ratios
 
--- scripts/story_adj_extraction.sh
+++ scripts/story_adj_extraction.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 BASE_DIR="$(dirname "$PWD")"  # Cultural-Fairness-LLM
 
-# python $BASE_DIR/generative/data-processing/adj_extraction.py \
-#         --input_file $BASE_DIR/generative/generated-stories/Llama-3.3-70B-Instruct-Turbo_Western.json \
-#         --output_dir $BASE_DIR/generative/extracted-adj
+python $BASE_DIR/generative/data-processing/adj_extraction.py \
+        --input_file $BASE_DIR/generative/generated-stories/Llama-3.3-70B-Instruct-Turbo_Western.json \
+        --output_dir $BASE_DIR/generative/extracted-adj
 
-# python $BASE_DIR/generative/data-processing/adj_extraction.py \
-#         --input_file $BASE_DIR/generative/generated-stories/Llama-3.3-70B-Instruct-Turbo_Vietnamese.json \
-#         --output_dir $BASE_DIR/generative/extracted-adj
+python $BASE_DIR/generative/data-processing/adj_extraction.py \
+        --input_file $BASE_DIR/generative/generated-stories/Llama-3.3-70B-Instruct-Turbo_Vietnamese.json \
+        --output_dir $BASE_DIR/generative/extracted-adj
 
 python $BASE_DIR/generative/data-processing/adj_extraction.py \
         --input_file $BASE_DIR/generative/generated-stories/GPT-4_Western.json \
@@ -15,4 +15,12 @@ python $BASE_DIR/generative/data-processing/adj_extraction.py \
 
 python $BASE_DIR/generative/data-processing/adj_extraction.py \
         --input_file $BASE_DIR/generative/generated-stories/GPT-4_Vietnamese.json \
+        --output_dir $BASE_DIR/generative/extracted-adj
+
+python $BASE_DIR/generative/data-processing/adj_extraction.py \
+        --input_file $BASE_DIR/generative/generated-stories/Gemini-1.5-flash_Western.json \
+        --output_dir $BASE_DIR/generative/extracted-adj
+
+python $BASE_DIR/generative/data-processing/adj_extraction.py \
+        --input_file $BASE_DIR/generative/generated-stories/Gemini-1.5-flash_Vietnamese.json \
         --output_dir $BASE_DIR/generative/extracted-adj
